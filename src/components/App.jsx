1e45a31756c5030dff581d1f9bafbad1a221913f
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-09-08 13:19:29 -0400

    Added DisplayNames; Built App.jsx and App.css; successful output of DisplayNames

---
+++ src/components/App.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import DisplayName from './DisplayName';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+        //Exercise 1
+            firstName: 'Reggie',
+            lastName: 'White',
+        
+        //Exercise 2 
+            names: ['Mike', 'Nevin', 'Aaron', 'Tory', 'Kelly'],
+
+        //Exercise 3
+            superheroes : [
+                {
+                    superheroId: 1,
+                    name: 'Batman',
+                    primaryAbility: 'Wealthy',
+                    secondarAbility: 'Rich'
+                },
+                {
+                    superheroId: 2,
+                    name: 'Superman',
+                    primaryAbility: 'Super strength',
+                    secondarAbility: 'Fly'
+                },
+                {
+                    superheroId: 3,
+                    name: 'Spiderman',
+                    primaryAbility: 'Spider senses',
+                    secondarAbility: 'Shoots web'
+                }
+            ]
+        };
+    }
+    render() { 
+        return ( 
+            <div>
+                <DisplayName firstName = {this.state.firstName} lastName = {this.state.lastName} />
+            </div>
+         )
+    }
+}
+ 
+export default App;
---
+++ src/components/DisplayName.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const DisplayName = (props) => {
+    return(
+        <h1>{props.firstName} {props.lastName}</h1>
+    );
+}
+
+export default DisplayName;
