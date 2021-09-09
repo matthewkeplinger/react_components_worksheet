024c476aacaa3a70ece1f470a8644a9129917a63
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-09-09 15:05:44 -0400

    Removed code from DisplayJoke that was commented out for testing; cleaned up all files

--- src/components/DisplayJoke/DisplayJoke.jsx
+++ src/components/DisplayJoke/DisplayJoke.jsx
@@ -1,45 +1,8 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-
-// class DisplayJoke extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = { 
-//             jokes: []
-//          }
-//     }
-
-//    //Worksheet 2, Jokes
-//    componentDidMount(){
-//     this.getJokes();
-//     }
-
-//     //get jokes API async
-//     async getJokes(){
-//         let response = await axios.get('https://v2.jokeapi.dev/joke/Programming?type=twopart&amount=2')
-//         this.setState({
-//             jokes: response.data.jokes
-//         });
-//     }
-//     render() { 
-//         return (
-//             <div>
-//                 {this.state.jokes.map(joke => {
-//                     return( 
-//                     <div>
-//                         <h1>{joke.setup}</h1><br/>
-//                         <h1>{joke.delivery}</h1><br/>
-//                     </div>)
-//                 })}
-//             </div>
-//          );
-//     }
-// }
+import React from 'react';
 
  const DisplayJoke = (props) => {
      return (
             <div>
-                {/* {this.state.jokes.map(joke => { */}
                 {props.jokes.map(joke => {
                     return( 
                     <div>
